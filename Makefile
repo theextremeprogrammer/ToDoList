23734ba60457f8806c782d8f420b9c2d9e84cac2
Derek Lee <dereklee@downtothis.com>
2018-11-06 18:11:08 +0900

    Adds Carthage dependencies for the app and for tests; confirms test dependencies working

--- Makefile
+++ Makefile
@@ -2,10 +2,10 @@ sort:
 	@perl ./bin/sortXcodeProject ToDoList.xcodeproj/project.pbxproj
 
 bootstrap:
-	@carthage bootstrap --platform iOS
+	@carthage bootstrap --platform iOS --no-use-binaries --use-ssh
 
 update:
-	@carthage update --platform iOS
+	@carthage update --platform iOS --no-use-binaries --use-ssh
 
 unit-tests:
 	@xcodebuild -project ToDoList.xcodeproj -scheme "ToDoList" -destination "platform=iOS Simulator,OS=12.0,name=iPhone 8" build test
--- ToDoListTests/Components/WiringTest.swift
+++ ToDoListTests/Components/WiringTest.swift
@@ -1,11 +1,24 @@
-import XCTest
+import Quick
+import Nimble
+import Succinct
 @testable import ToDoList
 
-class ToDoListTests: XCTestCase {
-    func testWiring() {
-        let twelve = 12
-        
-        
-        XCTAssertEqual(twelve, 12)
+class ToDoListTests: QuickSpec {
+    override func spec() {
+        describe("wiring test") {
+            it("works") {
+                let twelve = 12
+                
+                
+                expect(twelve).to(equal(12))
+            }
+            
+            it("can call Succinct methods too") {
+                let maybeObject: String? = nil
+                
+                
+                expect(maybeObject.isNil()).to(beTrue())
+            }
+        }
     }
 }
