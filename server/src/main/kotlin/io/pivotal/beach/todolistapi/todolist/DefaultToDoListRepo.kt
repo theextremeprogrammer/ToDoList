d3df6acc6eb4ac76ac0e07706c80dbacd8879854
Derek Lee <dereklee@downtothis.com>
2018-11-08 14:55:55 +0900

    Hard-codes some to-do items to allow the ability to build out the client.

--- server/src/main/kotlin/io/pivotal/beach/todolistapi/todolist/DefaultToDoListRepo.kt
+++ server/src/main/kotlin/io/pivotal/beach/todolistapi/todolist/DefaultToDoListRepo.kt
@@ -1,10 +1,15 @@
 package io.pivotal.beach.todolistapi.todolist
 
 import org.springframework.stereotype.Repository
+import java.util.Arrays.asList
 
 @Repository
 class DefaultToDoListRepo : ToDoListRepo {
     override fun getAll(): List<ToDoItem> {
-        return emptyList()
+        return asList(
+                ToDoItem(1L, "Pick up dry cleaning", false),
+                ToDoItem(2L, "Mail holiday cards", false),
+                ToDoItem(3L, "Buy birthday present for mom", false)
+        )
     }
 }
