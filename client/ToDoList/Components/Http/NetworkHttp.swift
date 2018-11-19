b4e2375008389edfa35237f668855938756f5564
Derek Lee <dereklee@downtothis.com>
2018-11-19 11:34:32 +0900

    Adds "why?" comments to the NetworkHttp and Reloader/Reloadable protocols for reference.

--- client/ToDoList/Components/Http/NetworkHttp.swift
+++ client/ToDoList/Components/Http/NetworkHttp.swift
@@ -13,10 +13,14 @@ struct NetworkHttp: Http {
     let networkSession: NetworkSession
 
     func get(url urlString: String) -> Future<Data, HttpError> {
+        // First create a promise so we can return the future from this method call. We can
+        //      then call success() or failure() when we receive a response with the appropriate
+        //      data or error that is received.
         let requestPromise = Promise<Data, HttpError>()
         
         let url = URL(string: urlString)!
         let urlRequest = URLRequest(url: url)
+        
         let _ = networkSession
             .dataTask(with: urlRequest) { (maybeData, maybeUrlResponse, maybeError) in
                 if let data = maybeData {
--- client/ToDoList/Components/Reloading/DefaultReloader.swift
+++ client/ToDoList/Components/Reloading/DefaultReloader.swift
@@ -1,7 +1,18 @@
+// This protocol is created to duplicate the reloadData() method declaration on
+//      UITableView and/or UICollectionView (and anything else that needs the
+//      ability to be re-loaded).
+//
+// The conformance of UITableView to this protocol is managed in a separate file
+//      under the UI because this requires importing UIKit - something that we
+//      will try to limit to just those files under the UI folder.
 protocol Reloadable {
     func reloadData()
 }
 
+// The Reloader is responsible for reloading a Reloadable object. This allows this
+//      functionality to be easily testable so we can pass in any UITableView (or
+//      UICollectionView, or any object that supports reloading) to confirm that
+//      this functionality is executed.
 protocol Reloader {
     func reload(reloadable: Reloadable)
 }
