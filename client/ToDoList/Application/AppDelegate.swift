d7aad5d4fbb69108e3f70e31415e3ae854e4f0f7
Derek Lee <dereklee@downtothis.com>
2018-11-19 11:14:00 +0900

    Adds some comments to the Makefile and AppDelegate to clarify the purpose of items

--- client/ToDoList/Application/AppDelegate.swift
+++ client/ToDoList/Application/AppDelegate.swift
@@ -7,12 +7,16 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var http: Http
     
     // MARK: - Initialization
+    
+    // This convenience initializer is called first when running the app and creates
+    //      our dependencies to inject them into the init(...) method.
     convenience override init() {
         let http = NetworkHttp(networkSession: URLSession.shared)
         
         self.init(http: http)
     }
-    
+
+    // This initializer can be called from tests for the purpose of testing the app delegate.
     init(http: Http) {
         self.http = http
     }
@@ -23,6 +27,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
         let toDoListRepo = NetworkToDoListRepository(http: http)
+
         let rootViewController = ToDoListViewController(
             toDoListRepo: toDoListRepo,
             reloader: DefaultReloader()
