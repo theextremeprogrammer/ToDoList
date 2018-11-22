23c577b38452c62b522e0536162f10f874f3224d
Derek Lee <dereklee@downtothis.com>
2018-11-22 09:36:04 +0900

    Pass real router from app delegate

--- client/ToDoList/Application/AppDelegate.swift
+++ client/ToDoList/Application/AppDelegate.swift
@@ -4,18 +4,22 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
 
     // MARK: - Injected Dependencies
-    var router: Router?
+    var router: Router
     
     // MARK: - Initialization
     
     // This convenience initializer is called first when running the app and creates
     //      our dependencies to inject them into the init(...) method.
     convenience override init() {
-        self.init()
+        let router = NavigationRouter(
+            navigationController: UINavigationController(),
+            animated: true
+        )
+        self.init(router: router)
     }
 
     // This initializer can be called from tests for the purpose of testing the app delegate.
-    init(router: Router? = nil) {
+    init(router: Router) {
         self.router = router
     }
     
@@ -24,10 +28,10 @@ final class AppDelegate: UIResponder, UIApplicationDelegate {
         _ application: UIApplication,
         didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool
     {
-        router?.showToDoListViewController()
+        router.showToDoListViewController()
 
         window = UIWindow()
-        window?.rootViewController = router?.navigationController
+        window?.rootViewController = router.navigationController
         window?.makeKeyAndVisible()
         
         return true
