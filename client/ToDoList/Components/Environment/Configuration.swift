ac19be95fb0cbcb2a727f371831e42ada7467597
Derek Lee <dereklee@downtothis.com>
2018-12-04 10:08:44 +0900

    The application should blow up if we cannot instantiate our configuration object.

--- client/ToDoList/Components/Environment/Configuration.swift
+++ client/ToDoList/Components/Environment/Configuration.swift
@@ -3,12 +3,8 @@ import Foundation
 struct Configuration {
     static var environment: Environment {
         get {
-            if let configuration = Bundle.main.object(forInfoDictionaryKey: "Configuration") as? String,
-                let environment = Environment.init(rawValue: configuration) {
-                return environment
-            }
-            
-            return Environment.Debug
+            let configuration = Bundle.main.object(forInfoDictionaryKey: "Configuration") as! String
+            return Environment.init(rawValue: configuration)!
         }
     }
 }
