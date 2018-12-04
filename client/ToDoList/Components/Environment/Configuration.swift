3de2ad9555f68311a04fd82d4eb8940259b80938
Derek Lee <dereklee@downtothis.com>
2018-12-05 08:47:07 +0900

    Adds comments to explain the Configuration and Environment object definitions

--- client/ToDoList/Components/Environment/Configuration.swift
+++ client/ToDoList/Components/Environment/Configuration.swift
@@ -3,7 +3,29 @@ import Foundation
 struct Configuration {
     static var environment: Environment {
         get {
+            /*  In the Application's Info.plist a new key has been added called "Configuration".
+             
+                This key is set to a hard-coded value of "$(CONFIGURATION)" which is an Xcode
+                build variable that will retrieve the name of the current build configuration
+                that is set for the selected scheme.
+             
+                Standard build configurations for all Xcode projects include "Debug" and
+                "Release". You can see all Configurations by accessing:
+             
+                Application -> Project -> <project name> -> Info tab -> Configurations
+             
+                The optional value is explicitly unwrapped as this should crash if this
+                value cannot be found.
+             */
             let configuration = Bundle.main.object(forInfoDictionaryKey: "Configuration") as! String
+            
+            /*  The string value of the configuration name is then passed as the raw value
+                for the Environment enum which contains the definitions for all currently
+                configured build configurations.
+
+                The optional value is explicitly unwrapped as this should crash if this
+                value cannot be found.
+             */
             return Environment.init(rawValue: configuration)!
         }
     }
--- client/ToDoList/Components/Environment/Environment.swift
+++ client/ToDoList/Components/Environment/Environment.swift
@@ -1,8 +1,16 @@
 enum Environment: String {
+    /*  The following valid values for this enum map directly to the build configuration
+        names as defined for this project in Xcode under:
+     
+        Application -> Project -> <project name> -> Info tab -> Configurations
+     */
     case Debug
     case Staging
     case Release
-    
+
+    /*  Any values which need to be configured differently per environment can then
+        be organized here.
+     */
     var baseURL: String {
         switch self {
         case .Debug: return "http://localhost:8080"
