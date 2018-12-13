52e6e040e22cbb1c3cc82db8f1f0e2e2b3fb4930
Derek Lee <dereklee@downtothis.com>
2018-12-13 15:29:28 +0900

    Back-fill testing that the delegate is set when navigating to the add to do item VC

--- client/ToDoListTests/Components/Router/NavigationRouterSpec.swift
+++ client/ToDoListTests/Components/Router/NavigationRouterSpec.swift
@@ -30,16 +30,28 @@ final class NavigationRouterSpec: QuickSpec {
                 expect(topVC).toEventually(beAKindOf(ToDoListViewController.self))
             }
 
-            it("shows the add to do item view controller as a modal") {
-                instantiateUIWindowForUnitTesting(rootViewController: rootNavController)
+            describe("showing the add to do item view controller") {
+                var spyAddToDoItemDelegate: SpyAddToDoItemDelegate!
 
-
-                navRouter.showAddToDoItemViewController(delegate: SpyAddToDoItemDelegate())
-
-
-                let presentedNavVC = rootNavController.presentedViewController as? UINavigationController
-                let presentedVC = presentedNavVC?.topViewController
-                expect(presentedVC).toEventually(beAKindOf(AddToDoItemViewController.self))
+                beforeEach {
+                    instantiateUIWindowForUnitTesting(rootViewController: rootNavController)
+                    spyAddToDoItemDelegate = SpyAddToDoItemDelegate()
+                    
+                    
+                    navRouter.showAddToDoItemViewController(delegate: spyAddToDoItemDelegate)
+                }
+                
+                it("displays as a modal") {
+                    let presentedNavVC = rootNavController.presentedViewController as? UINavigationController
+                    let presentedVC = presentedNavVC?.topViewController
+                    expect(presentedVC).toEventually(beAKindOf(AddToDoItemViewController.self))
+                }
+                
+                it("sets the delegate to the passed in object") {
+                    let presentedNavVC = rootNavController.presentedViewController as? UINavigationController
+                    let addToDoItemVC = presentedNavVC?.topViewController as? AddToDoItemViewController
+                    expect(addToDoItemVC?.delegate).to(be(spyAddToDoItemDelegate))
+                }
             }
 
             it("dismisses the presented modal view controller") {
@@ -51,7 +63,6 @@ final class NavigationRouterSpec: QuickSpec {
                 navRouter.dismissModalVC()
 
 
-                
                 expect(rootNavController.presentedViewController).toEventually(beNil())
             }
         }
